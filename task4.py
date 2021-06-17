fdc0795213555bcfe19a067be1165b00ed0a6061
volevanya <7323649aAqQ1!1>
2021-06-17 19:47:47 +0300

    Complete 4th task

---
+++ task4.py
@@ -0,0 +1,6 @@
+# 4. Создать переменные john_gender и marta_gender булевого типа. Пусть john будет принимать истинное значение а marta
+# ложное. Вывести в консоль методом print значения обеих переменных.
+
+john_gender = True
+marta_gender = False
+print(john_gender, marta_gender)
