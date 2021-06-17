6b9d5826bb34b47f53fdd4215ecceb5bc285cdce
volevanya <7323649aAqQ1!1>
2021-06-17 19:57:19 +0300

    Complete 5th task

---
+++ task5.py
@@ -0,0 +1,7 @@
+# 5. Создать переменные john_friends и marta_friends. Присвоить переменным списки. Каждый список должен содержать имена
+# дружей. У джона свой список друзей а у Марты свой. Друзья (имя друга) могут быть обычные строки “James”, “Peter”
+# и т. Д.
+
+john_friends = ["Alex", "Harry", "Ivan"]
+marta_friends = ["Hanna", "Rio", "Viki"]
+print(john_friends, marta_friends)
