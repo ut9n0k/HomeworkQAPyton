df48d3dcccbf01e7c880896fae30cc7cc4a64e8f
Masha M <mmishustina7@gmail.com>
2021-06-23 22:57:05 +0300

    ID: HW2
    # Complete 3rd task

---
+++ homework_2/task3.py
@@ -0,0 +1,7 @@
+# У вас есть 3 группы людей bigno_blacklist, poker_blacklist, majong_blacklist.
+# Имена людей в формате "John Dow" первое и второе имя. Найти тех кто состоит во всех черных списках.
+
+bigno_blacklist = {"John Dow", 'Marta Hey', 'Gaga Gaga'}
+poker_blacklist = {"John Dow", 'Harry Hai', 'Gugu Gaga'}
+majong_blacklist = {"John Dow", 'Hou Hou', 'Gigi Gigi'}
+print(bigno_blacklist.intersection(poker_blacklist, majong_blacklist))
