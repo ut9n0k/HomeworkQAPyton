ef6d3fc51de0c3e897ead4e40ad5f540d41b15c0
Masha M <mmishustina7@gmail.com>
2021-06-23 23:19:19 +0300

    ID: HW2
    # Complete 4th task

---
+++ homework_2/task4.py
@@ -0,0 +1,12 @@
+# У вас есть две группы людей. В одной группе состоят всеядные люди в другой лишь вегетарианцы.
+# Всеядный является вегетарианцем но вегетарианец не является всеядным.
+# Вывести список гостей которые могут есть овощи и зелень.
+
+love_all_meals = ['Jerry', 'Mark', 'Ivan', 'Hulio']
+love_only_veggies = ['Agatha', 'Dora', 'Hrisha']
+can_eat_veggies = []
+
+can_eat_veggies.extend(love_only_veggies)
+can_eat_veggies.extend(love_all_meals)
+
+print(can_eat_veggies)
