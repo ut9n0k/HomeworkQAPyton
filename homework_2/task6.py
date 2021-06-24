b13ded0897565b58c52709434b1600454fb85674
ut9n0k <mmishustina7@gmail.com>
2021-06-24 14:19:52 +0300

    final final 6th task

--- homework_2/task6.py
+++ homework_2/task6.py
@@ -3,19 +3,5 @@
 # Если в списке есть 2 джона нужно взять лишь одного из них.
 # "John Dow", "John Dow", "Marta Dow" => "John Dow", "Marta Dow")
 
-# list = ['John', 'Ivan', 'Marta', 'Marishka', 'Hou', 'John']
-# print(list.pop(0))
-
-list_1 = {
-    1: 'Harry',
-    2: 'Teo',
-    3: 'Leo',
-    4: 'Ivan'
-}
-
-list_2 = {
-    1: 'Garry',
-    2: 'Teo',
-    3: 'Leo',
-    4: 'Dio'
-}
+list = ['John', 'Ivan', 'Marta', 'Marishka', 'Hou', 'John']
+print(list.pop(0))
