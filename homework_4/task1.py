b096abde684f0838e508c3a5d74a91216cd56431
ut9n0k <mmishustina7@gmail.com>
2021-07-06 14:08:20 +0300

    ID: HW4 complete 1st task

---
+++ homework_4/task1.py
@@ -0,0 +1,6 @@
+# 1 есть строка с именами разделенная пробелами "john marta james Morgan Adam Maria huang" преобразовать
+# строку таким образом что бы каждое имя однозначно начиналось с большой буквы.
+
+names = "john marta james Morgan Adam Maria huang"
+new_names = names.title()
+print(new_names)
