5481b3fc2d53ea0f8b7ed46d53f47c56f1f6ce58
ut9n0k <mmishustina7@gmail.com>
2021-07-08 18:06:47 +0300

    HW4: draft 3rd task

--- homework_4/task3.py
+++ homework_4/task3.py
@@ -1,15 +1,26 @@
 # 3 есть строка переданная в качестве квери параметров
 # "     name=Amanda=sssss&age=32&&salary=1500&currency=quro                             ".
-# Преобразовать эту строку в словарь где ключем должно быть значение перед =, а значение пары значение после равно
-# {name: Amanda......}
+# Преобразовать эту строку в словарь где ключем должно быть значение перед =
+# а значение пары значение после равно {name: Amanda......}
 
-query = "     name=Amanda=sssss&age=32&&salary=1500&currency=quro                             "
+query = "     name=Amanda=sssss&age=32&&salary=1500&currency=quro                             ".strip()
 my_dict = {}
-seperated = query.split('&')
+pairs = (query.split('&'))
 
-dict(my_dict.split('=') for my_dict in query.split())
+#for pair in pairs:
+#     print(pair.split('=', maxsplit=1))
 
-for pare in seperated:
+# dict(pairs.split('=') for pairs in str.split())
 
-    my_dict.items()
-print(my_dict)
+
+
+
+
+
+
+
+
+# dict(my_dict.split('=') for my_dict in query.split())
+# for pare in seperated:
+#    my_dict.items()
+# print(my_dict)
