7aa0fb270faeb65028895baf9ce8365ba4de61c2
Masha M <mmishustina7@gmail.com>
2021-07-21 18:24:46 +0300

    HW6: complete 1st task

--- homework_6/task1.py
+++ homework_6/task1.py
@@ -4,18 +4,46 @@
 # Описать функцию в приложенном файле таким образом что бы все проверки в __main__ блоке task_1 выполнялись корректно.
 # НЕ НУЖНО ВЫЗЫВАТЬ ФУНКЦИЮ САМИМ Я ЭТО УЖЕ СДЕЛАЛ В УТВЕРЖДЕНИЯХ "assert".
 
-def arithmetic(a, b, operation):
+def arithmetic(left_operand, right_operand, operation):
+    """
+        Apply arithmetic operation for provided left and right operands
+    """
 
     if operation == "+":
-        return a + b
+        return left_operand + right_operand
     elif operation == "-":
-        return a - b
+        return left_operand - right_operand
     elif operation == "*":
-        return a * b
+        return left_operand * right_operand
     elif operation == "/":
-        return a / b
+        return left_operand / right_operand
     else:
-        print(f"Not known operation: {operation}")
+        return f"Not known operation: {operation}"
 
 
-print(arithmetic(7, 7, "+"))
+if __name__ == "__main__":
+    assert arithmetic(3, 4, operation="*") == 12
+    assert arithmetic(3, 4, operation="+") == 7
+    assert arithmetic(25, 5, operation="/") == 5
+    assert type(arithmetic(25, 5, operation="/")) == float
+    assert arithmetic(5, 5, operation="//") == f"Not known operation: //"
+    assert arithmetic.__doc__ == (
+        f"\n{' ' * 8}"
+        f"Apply arithmetic operation for provided left and right operands\n"
+        f"{' ' * 4}"""
+    )
+    assert arithmetic.__code__.co_name == "arithmetic"
+    assert arithmetic.__code__.co_varnames == ("left_operand", "right_operand", "operation")
+    try:
+        arithmetic(1, 2, 3)
+    except TypeError as e:
+        assert e.__class__ is TypeError
+    try:
+        arithmetic(left_operand=1, right_operand=2, operation="+")
+    except TypeError as e:
+        assert e.__class__ is TypeError
+
+    try:
+        arithmetic(1, right_operand=2, operation="+")
+    except TypeError as e:
+        assert e.__class__ is TypeError
