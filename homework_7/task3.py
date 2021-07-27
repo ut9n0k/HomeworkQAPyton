1b1bbff04f0c8678253f85cd8fe77d8929270778
Nikita Limarchuk <nikita.limarchuk@gmail.com>
2021-07-27 23:39:20 +0300

    HW7: add comment in task 3 for reduce with solution

--- homework_7/task3.py
+++ homework_7/task3.py
@@ -20,21 +20,16 @@ def reduce_for_sequences(callback, sequence: Union[list, tuple, dict]) -> Union[
 
 
 if __name__ == "__main__":
-    bob_info = {
-        "name": "Bob",
-        "lastname": "Bobina",
-        "age": 44,
-        "salary": 2200.22
-    }
-    ben_info = {
-        "name": "Ben",
-        "lastname": "Benini",
-        "age": 22,
-        "salary": 400.44
-    }
-# for list
-print(reduce_for_sequences(lambda num: num ** 2, [10, 200, 3003, 40.44, 500.55]))
-# for tuple
-print(reduce_for_sequences(lambda name: f"Hello, {name}! How are you?", ("Bernard", "Brittani", "Bob", "John")))
-# for dict
-print("Sorry, but I don't know how this method works with dicts, so I passed it. Don't be angry, please T-T")
+    # Well good but main purpose of reduce in other things. Take a look how is look like reduce
+    # def custom_reduce(callback: Callable, sequence):
+    #     element = sequence[0]
+    #     counter = 0
+    #     for _ in sequence:
+    #         if len(sequence) - 1 == counter:
+    #             break
+    #         counter += 1
+    #         element = callback(element, sequence[counter])
+    #     return element
+
+    # custom_reduce(lambda a, b: a + b, [1, 2, 3, 4, 5])
+    pass
