a1235b272671e0fa235a87666cecd62acd38284e
Masha M <mmishustina7@gmail.com>
2021-07-27 14:20:14 +0300

    HW7: complete 1st task

--- homework_7/task1.py
+++ homework_7/task1.py
@@ -5,64 +5,37 @@
 # В качестве последовательностей могут быть переданы списки, кортэжи, словари.
 # Буду проверять с помощью майпай аннотации.
 
-from typing import Union
+from typing import Union, Callable, List, Tuple, Dict
 
 """
-    Function with custom filtering
+    Function for sequences filtering
 """
 
 
-def filter_for_sequences(callback, sequence: Union[list, tuple, dict]) -> Union[list, tuple, dict, str]:
+def filter_for_sequences(callback: Callable, sequence: Union[List, Tuple, Dict]):
     result = []
-    if type(sequence) == list:
-        for item in sequence:
-            if callback(item):
-                result.append(item)
-        return result
-
-    elif type(sequence) == tuple:
-        for item in sequence:
-            if callback(item):
-                result.append(item)
-        return tuple(result)
-
-    elif type(sequence) == dict:
-        new_dict = {}
-        for key, value in sequence.items():
-            if callback(value):
-                result.append(value)
-                new_dict[key] = value
-        return new_dict
-
-    else:
-        return f"Sorry, your sequence has unsupported type"
+    for items in sequence:
+        result.append(items) if callback(items) else None
+    return result
 
 
 if __name__ == "__main__":
-    my_list = [10, 200, 3003, 40.44, 500.55]
-    my_tuple = ("Bernard", "Brittani", "Bob", "John", "Harold", "Jenny", "Ben")
-    my_dict = {
-        "Bob": {
-            "name": "Bob",
-            "lastname": "Bobina",
-            "age": 44,
-            "salary": 2200.22
-        },
-        "Ben": {
-            "name": "Ben",
-            "lastname": "Bebina",
-            "age": 22,
-            "salary": 400.44
-        }
+    bob_info = {
+        "name": "Bob",
+        "lastname": "Bobina",
+        "age": 44,
+        "salary": 2200.22
     }
-    my_suit = {"Hello", "Bye"}
+    ben_info = {
+        "name": "Ben",
+        "lastname": "Benini",
+        "age": 22,
+        "salary": 400.44
+    }
 
     # for list
-    print(filter_for_sequences(lambda num: num % 2 == 0, my_list))
+    print(filter_for_sequences(lambda num: num % 2 == 0, [10, 200, 3003, 40.44, 500.55]))
     # for tuple
-    print(filter_for_sequences(lambda word: word.startswith('H'), my_tuple))
+    print(filter_for_sequences(lambda word: word.startswith('B'), ("Bernard", "Brittani", "Bob", "John")))
     # for dict
-    print(filter_for_sequences(lambda item: item["age"] == 44, my_dict))
-    print(filter_for_sequences(lambda item: item["name"].endswith('n'), my_dict))
-    # for suit
-    print(filter_for_sequences(lambda num: num % 2 == 0, my_suit))
+    print(filter_for_sequences(lambda item: item["name"].endswith('n'), [bob_info, ben_info]))
