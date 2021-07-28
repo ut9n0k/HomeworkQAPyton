c24edc62324364309cd8a33e79f3ef5c83bf5746
Masha M <mmishustina7@gmail.com>
2021-07-28 16:35:35 +0300

    HW8: complete 2nd task

---
+++ homework_8/task2.py
@@ -0,0 +1,16 @@
+# Создайте функцию которая способна вернуть квадраты четных элементов для диапазона  от 0 до 1000000000 включительно.
+# Решение должно отрабатывать и не фризить ваш комп.
+
+def my_gen():
+    num = 0
+    while num <= 1000000000:
+        yield num ** 2
+        num += 2
+
+
+range_square = my_gen()
+
+print(next(range_square))
+print(next(range_square))
+print(next(range_square))
+print(next(range_square))
