c4f3887e92461eada98075745912d208b530fe5e
Masha M <mmishustina7@gmail.com>
2021-08-14 13:55:59 +0300

    HW13: complete 2nd task

---
+++ homework_13/task2/Train.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from homework_13.task2.TrainCar import TrainCar
+
+
+class Train:
+    def __init__(self, locomotive_num: int):
+        self.__locomotive_num = locomotive_num
+        self.__traincars: List[TrainCar] = list()
+
+    def add_traincar(self, traincar: TrainCar):
+        return self.__traincars.append(traincar)
+
+    @property
+    def __len__(self):
+        return len(self.__traincars)
+
+    @property
+    def traincars(self):
+        return self.__traincars
+
+    @property
+    def locomotive_num(self):
+        return len(self.__traincars) - 1
