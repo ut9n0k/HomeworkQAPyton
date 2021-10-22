76100b20f75f57e8c22d3082a295d0decb586d2c
MykytaLymarchukDevPro <mykyta.lymarchuk@dev.pro>
2021-10-22 22:19:07 +0300

    HW20: almost right

--- homework_20/xml_movie_parser.py
+++ homework_20/xml_movie_parser.py
@@ -33,5 +33,47 @@ class Movie:
                                     return final
 
 
+# class Movie:
+#     def __init__(
+#         self,
+#         title: str,
+#         format: str,
+#         year: int,
+#         rating: str,
+#         description: str
+#     ):
+#         self.title = title
+#         self.format = format
+#         self.year = year
+#         self.rating = rating
+#         self.description = description
+#
+#     @classmethod
+#     def from_xml(cls, path: str) -> List[Movie]:
+#         tree = ElementTree.parse(path)
+#         collection = tree.getroot()
+#         movies = []
+#
+#         for movie in collection.iter('movie'):
+#             data = {}
+#             data["title"] = movie.attrib["title"]
+#
+#             for child in movie:
+#                 data[child.tag] = child.text
+#             movies.append(cls(**data))
+#         return movies
+#
+#     def __str__(self):
+#         data = ""
+#         for key, value in self.__dict__.items():
+#             data += f"\t{key}: {value}\n"
+#         return f"{{\n{data[:len(data) - 1]}\n}}"
+#
+#
+# if __name__ == '__main__':
+#     movies = Movie.from_xml("market.xml")
+#     for movie in movies:
+#         print(movie)
+
 if __name__ == '__main__':
     movies = Movie.from_xml("market.xml")
